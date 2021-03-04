d44329ae9170fb573d5fbf024732c62979e75271
DiFalP <dima2000-14@mail.ru>
2021-03-05 00:08:02 +0300

    Homework 1

---
+++ java/server/ClientHandler.java
@@ -0,0 +1,78 @@
+package server;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Обработчик входящих клиентов
+ */
+public class ClientHandler implements Runnable {
+	private final Socket socket;
+
+	public ClientHandler(Socket socket) {
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		try (DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		     DataInputStream in = new DataInputStream(socket.getInputStream())){
+			while (true) {
+				String command = in.readUTF();
+				if ("upload".equals(command)) {
+					try {
+						File file = new File("server" + File.separator + in.readUTF());
+						if (!file.exists()) {
+							file.createNewFile();
+						}
+						long size = in.readLong();
+						FileOutputStream fos = new FileOutputStream(file);
+						byte[] buffer = new byte[256];
+						for (int i = 0; i < (size + 255) / 256; i++) {
+							int read = in.read(buffer);
+							fos.write(buffer, 0, read);
+						}
+						fos.close();
+						out.writeUTF("DONE");
+					} catch (Exception e) {
+						out.writeUTF("ERROR");
+					}
+				} else if ("download".equals(command)) {
+					// TODO: 02.03.2021
+					try {
+						File file = new File("client" + File.separator + in.readUTF());
+						if (!file.exists()) {
+							file.createNewFile();
+						}
+						long size = in.readLong();
+						FileOutputStream fos = new FileOutputStream(file);
+						byte[] buffer = new byte[256];
+						for (int i = 0; i < (size + 255) / 256; i++) {
+							int read = in.read(buffer);
+							fos.write(buffer, 0, read);
+						}
+						fos.close();
+						out.writeUTF("Download from server");
+					} catch (Exception e) {
+						out.writeUTF("ne download");
+					}
+				} else if ("remove".equals(command)) {
+					// TODO: 02.03.2021
+					try {
+						File file = new File("server" + File.separator + in.readUTF());
+						if (!file.exists()) {
+							out.writeUTF("File is not exists");
+						}
+						file.delete();
+						out.flush();
+						out.writeUTF("Remove from server");
+					} catch (Exception e) {
+						out.writeUTF("ne remove");
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
